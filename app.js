916e28fc0d9634013a5fa2b44b27e51c69160ab0
sedraismael <sedra.i_akn@aknschool.com>
2025-04-16 08:59:38 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VintagePhotoBooth from "./components/VintagePhotoBooth";
+
+function App() {
+  return (
+    <div className="App">
+      <VintagePhotoBooth />
+    </div>
+  );
+}
+
+export default App;
